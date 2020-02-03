5a18b4f2a57c1e7308b1a1e7f5dc8242f30be7cc
Adokiye <gorgeceo@gmail.com>
2020-02-03 09:09:09 +0100

    SecurityFixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { initializeFirebase } from './push-notification';
+//import { initializeFirebase } from './push-notification';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-initializeFirebase();registerServiceWorker();
+//initializeFirebase();
+registerServiceWorker();
