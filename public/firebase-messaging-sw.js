ede44d421ff1e14f9f1fbd18a2a6cc08651aa54d
Adokiye <gorgeceo@gmail.com>
2020-02-03 09:26:24 +0100

    Fixes forfirebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-if (!firebase.apps.length) {
-    firebase.initializeApp({
-        apiKey: "AIzaSyB_yoY8Q1kXDfwqWCXnIxZ-2rmSRGS6ONM",
-        authDomain: "greenworld-laundry.firebaseapp.com",
-        databaseURL: "https://greenworld-laundry.firebaseio.com",
-        projectId: "greenworld-laundry",
-        storageBucket: "greenworld-laundry.appspot.com",
-        messagingSenderId: "501281460370",
-        appId: "1:501281460370:web:4712284e20156c6768564b",
-        measurementId: "G-C7T1KGH5ZH"
-      });
- }
+// if (!firebase.apps.length) {
+//     firebase.initializeApp({
+//         apiKey: "AIzaSyB_yoY8Q1kXDfwqWCXnIxZ-2rmSRGS6ONM",
+//         authDomain: "greenworld-laundry.firebaseapp.com",
+//         databaseURL: "https://greenworld-laundry.firebaseio.com",
+//         projectId: "greenworld-laundry",
+//         storageBucket: "greenworld-laundry.appspot.com",
+//         messagingSenderId: "501281460370",
+//         appId: "1:501281460370:web:4712284e20156c6768564b",
+//         measurementId: "G-C7T1KGH5ZH"
+//       });
+//  }
 
-const messaging = firebase.messaging();
-self.addEventListener('notificationclick', (event) => {
-    if (event.action) {
-        clients.openWindow(event.action);
-    }
-    event.notification.close();
-}); 
+// const messaging = firebase.messaging();
+// self.addEventListener('notificationclick', (event) => {
+//     if (event.action) {
+//         clients.openWindow(event.action);
+//     }
+//     event.notification.close();
+// }); 
