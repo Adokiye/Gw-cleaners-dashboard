2ebb3a057954a5789d6ebc89b72f04052c9a021f
Adokiye <gorgeceo@gmail.com>
2020-02-09 16:48:23 +0100

    Change port

--- src/root.js
+++ src/root.js
@@ -1,4 +1,4 @@
-export const API_URL= 'http://washnbox.com::3001/'  //'http://localhost:9001/'          //'https://whispering-escarpment-09374.herokuapp.com/'   //'https://whispering-escarpment-09374.herokuapp.com/'                 // 'http://10.0.2.2:3000/'         //          
+export const API_URL= 'http://washnbox.com:3001/'  //'http://localhost:9001/'          //'https://whispering-escarpment-09374.herokuapp.com/'   //'https://whispering-escarpment-09374.herokuapp.com/'                 // 'http://10.0.2.2:3000/'         //          
 export const APPLICATION_ID='sq0idp-ZWFqS07Li8TlWXwmwkZDoA'
 export const LOCATION_ID='PR3XJQ6YASSSN'
 // https://whispering-escarpment-09374.herokuapp.com/ | https://git.heroku.com/whispering-escarpment-09374.git
